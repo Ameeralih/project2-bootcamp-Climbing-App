e8f63f6d1b898e8aa41490df0ada070fe8359d6c
0mgABear <yeoyi94@gmail.com>
2022-11-24 16:03:33 +0800

    refactored all components into functional components

--- src/gym-page/CreateNewReview.jsx
+++ src/gym-page/CreateNewReview.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 
-export class CreateNewReview extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+export function CreateNewReview() {
+  return <></>;
 }
