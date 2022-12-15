eb84ff1cf834192701e5a2125813dcb6fe6faef2
ameeralih <ameerali.hassanali@gmail.com>
2022-12-15 13:59:38 +0800

    added maps

--- src/components/GymList.jsx
+++ src/components/GymList.jsx
@@ -1,16 +1,23 @@
 import { fetchGyms } from "../gymdata";
-import { Link, useSearchParams } from "react-router-dom";
+import { Link, useSearchParams, useNavigate } from "react-router-dom";
 import { Paper } from "@mui/material";
 import "../App.css";
 import { MapsAPI } from "./MapsAPI";
 import { Map } from "./MapsAPI";
 
-export function GymList() {
+export function GymList({ user }) {
   const gyms = fetchGyms();
   let [searchParams, setSearchParams] = useSearchParams();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!user) navigate("/login");
+  }, [user]);
 
   return (
     <>
+      <br />
+      <br />
       <br />
 
       <br />
@@ -42,7 +49,12 @@ export function GymList() {
           .map((gym) => (
             <li key={gym.slug}>
               <Link to={gym.slug}>
-                <Paper elevation={4} square={false} variant="elevation">
+                <Paper
+                  elevation={4}
+                  key={gym.name}
+                  square={false}
+                  variant="elevation"
+                >
                   {gym.name}
                 </Paper>
               </Link>
@@ -50,70 +62,6 @@ export function GymList() {
             </li>
           ))}
       </ul>
-      <br />
-      <br />
-      <br />
-    </>
-  );
-}
-import { fetchGyms } from "../gymdata";
-import { Link, useSearchParams, useNavigate } from "react-router-dom";
-import { Paper } from "@mui/material";
-import "../App.css";
-import { useEffect } from "react";
-
-export function GymList({ user }) {
-  const gyms = fetchGyms();
-  let [searchParams, setSearchParams] = useSearchParams();
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    if (!user) navigate("/login");
-  }, [user]);
-
-  return (
-    <>
-      <br />
-      <br />
-      <br />
-      <label>
-        <b>Search:</b>
-      </label>
-      <input
-        value={searchParams.get("filter") || ""}
-        onChange={(event) => {
-          let filter = event.target.value;
-          if (filter) {
-            setSearchParams({ filter });
-          } else {
-            setSearchParams({});
-          }
-        }}
-      />
-      <br />
-      <br />
-      {gyms
-        .filter((gym) => {
-          let filter = searchParams.get("filter");
-          if (!filter) return true;
-          let name = gym.name.toLowerCase();
-          return name.startsWith(filter.toLowerCase());
-        })
-        .map((gym) => (
-          <>
-            <Link to={gym.slug}>
-              <Paper
-                elevation={4}
-                key={gym.name}
-                square={false}
-                variant="elevation"
-              >
-                {gym.name}
-              </Paper>
-            </Link>
-            <br />
-          </>
-        ))}
       <br />
       <br />
       <br />
